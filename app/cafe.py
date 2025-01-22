3c3507fdbf2ebe7e0118824efac69e67b80069b2
Bohdan <khubashvili12@gmail.com>
2025-01-22 22:14:10 +0200

    Fix!

--- app/cafe.py
+++ app/cafe.py
@@ -1,20 +1,18 @@
 import datetime
-from errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
 
 
 class Cafe:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor):
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor['name']} is not vaccinated.")
-
-        expiration_date = visitor["vaccine"].get("expiration_date")
-        if expiration_date < datetime.date.today():
-            raise OutdatedVaccineError(f"{visitor['name']}'s vaccine is outdated.")
-
+            raise NotVaccinatedError("Visitor is not vaccinated.")
+        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            raise OutdatedVaccineError("Visitor's vaccine is outdated.")
         if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError(f"{visitor['name']} is not wearing a mask.")
-
+            raise NotWearingMaskError("Visitor is not wearing a mask.")
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -4,23 +4,23 @@ from app.errors import OutdatedVaccineError
 from app.errors import NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    masks_to_buy = 0
-
-    for friend in friends:
-        try:
-            cafe.visit_cafe(friend)
-        except NotVaccinatedError:
-            return "All friends should be vaccinated"
-        except OutdatedVaccineError:
-            return "'All friends should have up-to-date vaccinations"
-        except NotWearingMaskError:
-            masks_to_buy += 1
-
-    if masks_to_buy > 0:
-        return f"Friends should buy {masks_to_buy} masks"
-
-    return f"Friends can go to {cafe.name}"
+def go_to_cafe(friends: list, cafe: str) -> str:
+    masks_needed = 0
+    try:
+        for friend in friends:
+            try:
+                cafe.visit_cafe(friend)
+            except NotVaccinatedError:
+                return "All friends should be vaccinated"
+            except OutdatedVaccineError:
+                return "All friends should be vaccinated"
+            except NotWearingMaskError:
+                masks_needed += 1
+        if masks_needed > 0:
+            return f"Friends should buy {masks_needed} masks"
+        return f"Friends can go to {cafe.name}"
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == "__main__":
